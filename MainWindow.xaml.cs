441d6a1bc0044c706e5ef34637c580788555258e
Balal Shah <balal_shah94@hotmail.co.uk>
2019-01-24 16:37:58 +0000

    Add files via upload
    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+using System.Diagnostics;
+using System.Windows.Threading;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PerformanceCounter perfCPU = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        System.Windows.Threading.DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Start();
+
+            GetProcessorInfo();
+            GetRamInfo();
+            CenterWindowOnScreen();
+        }
+
+        private void CenterWindowOnScreen()
+        {
+            var screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            var screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            var windowWidth = this.Width;
+            var windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            rpbCPU.Value = (int)perfCPU.NextValue();
+            lblCPUPercent.Text = "CPU ";
+        }
+
+        private void RpbCPU_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region // Processor Info
+
+        private void GetProcessorInfo()
+        {
+            var wmi = new System.Management.ManagementClass("Win32_Processor");
+            var providers = wmi.GetInstances();
+
+            foreach (var provider in providers)
+            {
+                var processorFamily = (provider["Name"]);
+                var processorSpeed = Convert.ToInt32(provider["CurrentClockSpeed"]);
+                var processorStatus = provider["Status"].ToString();
+                var processorCores = Convert.ToInt16(provider["NumberOfLogicalProcessors"]);
+                var processorCache = (provider["Description"]);
+                var processorType = (provider["SystemName"]);
+
+
+
+
+                lblProcessorStatus.Text = "Processor Status : " + "  " + processorStatus.ToString();
+                lblProcessorClockSpeed.Text = "Clock Speed : " + "  " + processorSpeed.ToString() + " " + "MHz";
+                lblProcessorFamily.Text = processorFamily.ToString();
+                lblProcessorVoltage.Text = "Cores : " + "  " + processorCores.ToString();
+                lblProcessorCache.Text = processorCache.ToString();
+                lblProcessorType.Text = processorType.ToString();
+
+
+
+            }
+        }
+        #endregion
+
+        #region // Ram info
+
+        private void GetRamInfo()
+        {
+            var wmi = new System.Management.ManagementClass("Win32_MemoryDevice");
+            var providers = wmi.GetInstances();
+
+            foreach (var provider in providers)
+            {
+                var ramCapacity = Convert.ToInt16(provider["Availability"]);
+
+
+//                lblRAMSize.Text = ramCapacity.ToString();
+            }
+        }
+
+        #endregion
+    }
+
+
+
+}
---
+++ ProgressBar.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for ProgressBar.xaml
+    /// </summary>
+    public partial class ProgressBar : UserControl
+    {
+        public static readonly DependencyProperty IndicatorBrushProperty = DependencyProperty.Register("IndicatorBrush", typeof(Brush), typeof(ProgressBar));
+
+        public Brush IndicatorBrush
+        {
+            get { return (Brush)this.GetValue(IndicatorBrushProperty); }
+            set { this.SetValue(IndicatorBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundBrushProperty = DependencyProperty.Register("BackgroundBrush", typeof(Brush), typeof(ProgressBar));
+
+        public Brush BackgroundBrush
+        {
+            get { return (Brush)this.GetValue(BackgroundBrushProperty); }
+            set { this.SetValue(BackgroundBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProgressBrushBrushProperty = DependencyProperty.Register("ProgressBorderBrush", typeof(Brush), typeof(ProgressBar));
+
+        public Brush ProgressBorderBrush
+        {
+            get { return (Brush)this.GetValue(ProgressBrushBrushProperty); }
+            set { this.SetValue(ProgressBrushBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(ProgressBar));
+
+        public int Value
+        {
+            get { return (int)this.GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+
+
+
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+
+    [ValueConversion(typeof(int), typeof(double))]
+    public class ValueToAngle : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)(((int)value * 0.01) * 360);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)(((double)value / 360) * 100);
+        }
+    }
+}
